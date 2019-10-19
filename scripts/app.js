7acb9ab99c879021f73f4fa7354bdfde981e3206
Tibo van craenenbroeck <tibo.van.craenenbroeck@student.howest.be>
2019-10-19 15:29:38 +0200

    v5

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,27 @@
-const handleFloatingLabel = function() {};
-document.addEventListener('DOMContentLoaded', function() {
-	console.log('Script loaded!');
-	handleFloatingLabel();
+const handleEventListener = function(domLabel, domInput) {
+  if (domInput.value.length == 0) {
+    // Remove class
+    domLabel.classList.remove("is-floating");
+  } else {
+    //Add class
+    domLabel.classList.add("is-floating");
+  }
+};
+const handleFloatingLabel = function() {
+  const domFloatingLabelPassword = document.querySelector(
+    ".js-label-word__password"
+  );
+  const dominputPassword = document.querySelector(".js-password-input");
+  dominputPassword.addEventListener("blur", function() {
+    handleEventListener(domFloatingLabelPassword, dominputPassword);
+  });
+  const domFloatingLabelInput = document.querySelector(".js-label-word__input");
+  const dominputInput = document.querySelector(".js-username-input");
+  dominputInput.addEventListener("blur", function() {
+    handleEventListener(domFloatingLabelInput, dominputInput);
+  });
+};
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Script loaded!");
+  handleFloatingLabel();
 });
